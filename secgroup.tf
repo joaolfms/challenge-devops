40dfe59a8671225bb58d1801ecf969beb0a36f4d
Joao Lucas <jhonfmsvencedor@gmail.com>
2022-06-13 15:53:27 -0300

    Atualizando regras sec-group

--- secgroup.tf
+++ secgroup.tf
@@ -9,6 +9,24 @@ resource "aws_security_group_rule" "public_in_ssh" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
+  cidr_blocks       = ["201.50.35.252/32"]
+  security_group_id = aws_security_group.public.id
+}
+
+resource "aws_security_group_rule" "public_in_ssh" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.public.id
+}
+
+resource "aws_security_group_rule" "public_in_ssh" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.public.id
 }
